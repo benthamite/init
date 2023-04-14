b5f351c961d2133e75cb01b3a50fb1fb9fb0f325
Pablo Stafforini <pablo@stafforini.com>
2023-04-14 14:56:20 -0300

    tlon-init-build: move tlon-init-set-tangle-options earlier

--- tlon-init.el
+++ tlon-init.el
@@ -140,13 +140,13 @@ example, the default will be overridden by that code."
       (tlon-init-available-init-dirs)))))
   (unless (string-equal major-mode "org-mode")
     (user-error "Error: cannot build init from a buffer that is not visiting an `org-mode' file"))
+  ;; set tangle options
+  (tlon-init-set-tangle-options init-dir)
   ;; conditionally tangle extra config file
   (unless (user-pablo-p)
     (tlon-init-tangle-extra-config-file init-dir))
-  ;; set tangle options
-  (tlon-init-set-tangle-options init-dir)
   ;; tangle `config.org'
-  (tlon-init-tangle init-dir)
+  (tlon-init-tangle)
   ;; conditionally tangle extra config file again
   ;; TODO: implement more elegant solution; this is
   ;; a hack to deal with precedence problems
