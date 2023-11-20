f33042ebbf1690b5efb1566691c071e673eec770
Pablo Stafforini <pablo@stafforini.com>
2023-11-20 21:42:48 +0100

    Quote name of file in comment

--- tlon-init-without-overrides.el
+++ tlon-init-without-overrides.el
@@ -27,7 +27,7 @@ not by their position in `tlon-init-filenames'.
 (tlon-init-set-paths)
 (tlon-init-load-code-overrides)
 (tlon-init-set-tangle-flags user-emacs-directory)
-;; Continue the init process, loading the files defined above, in tlon-init-filenames
+;; Continue the init process, loading the files defined above, in `tlon-init-filenames'
 (dolist (key '(:init-1 :init-2 :init-3 :init-4 :init-5))
   (let ((fname (eval (alist-get key tlon-init-filenames nil))))
     (if fname (load fname))
