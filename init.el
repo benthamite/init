7c5cac0b58a80457f37bb990aaaa1f854c0ff49d
Pablo Stafforini <pablo@stafforini.com>
2023-11-24 11:22:32 +0100

    Comment out (setq tlon-init-boot-as-if-not-pablo t) in init.el

--- init.el
+++ init.el
@@ -1,3 +1,3 @@
 (load (file-name-concat user-emacs-directory "elpaca/repos/tlon-init/tlon-init.el"))
-(setq tlon-init-boot-as-if-not-pablo t) ; uncomment for testing in Pablo’s computer
+;; (setq tlon-init-boot-as-if-not-pablo t) ; uncomment for testing in Pablo’s computer
 (tlon-init-startup)
